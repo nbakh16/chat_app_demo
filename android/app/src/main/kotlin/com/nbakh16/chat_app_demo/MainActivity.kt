e285fa862effd29f9e7eea2e912e09659228d7ed
Nabil Akhunjee <nbakh16@gmail.com>
2024-01-10 11:09:32 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/nbakh16/chat_app_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nbakh16.chat_app_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:chat_app_demo/view/login_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chit Chat',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginView(),
+    );
+  }
+}
