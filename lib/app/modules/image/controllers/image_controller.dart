950c0b6f4aef2a58829b90d997e452f4dc2bfa59
Nabil Akhunjee <nbakh16@gmail.com>
2024-01-21 19:36:26 +0600

    animation for image zoom in/out.

--- lib/app/modules/image/controllers/image_controller.dart
+++ lib/app/modules/image/controllers/image_controller.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ImageController extends GetxController {
+class ImageController extends GetxController
+    with GetSingleTickerProviderStateMixin {
   static ImageController get to => Get.find();
 
   final String image = Get.arguments;
@@ -16,6 +17,8 @@ class ImageController extends GetxController {
   TransformationController transformationController =
       TransformationController();
   TapDownDetails? tapDownDetails;
+  late AnimationController animationController;
+  Animation<Matrix4>? animation;
 
   void doubleTapZoom() {
     final position = tapDownDetails!.localPosition;
@@ -29,6 +32,30 @@ class ImageController extends GetxController {
     final value = transformationController.value.isIdentity()
         ? zoomed
         : Matrix4.identity();
-    transformationController.value = value;
+    // transformationController.value = value;
+    animation =
+        Matrix4Tween(begin: transformationController.value, end: value).animate(
+      CurveTween(curve: Curves.easeOut).animate(animationController),
+    );
+
+    animationController.forward(from: 0);
+  }
+
+  @override
+  void onInit() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    )..addListener(() {
+        transformationController.value = animation!.value;
+      });
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    animationController.dispose();
+    transformationController.dispose();
+    super.onClose();
   }
 }
